d0621ab2f11a327a7ff7ebe66cf06e3440f15ae9
Dmitry Chirin <chirin.dima@gmail.com>
2023-02-24 00:44:58 +0100

    median working

--- app/Main.hs
+++ app/Main.hs
@@ -18,9 +18,45 @@ fib_tail n a b = fib_tail (n - 1) b (a + b)
 
 reverse_list :: [a] -> [a] -> [a]
 reverse_list [] new_list = new_list
-reverse_list old_list new_list = reverse_list (tail old_list) ((head old_list):new_list)
+reverse_list old_list new_list = reverse_list (tail old_list) ((head old_list) : new_list)
+
+convert_strings_to_lengths :: [String] -> [Int] -> [Int]
+convert_strings_to_lengths [] ints = ints
+convert_strings_to_lengths strings ints = convert_strings_to_lengths (tail strings) (length (head strings) : ints)
+
+get_median_ints :: [Int] -> Float
+get_median_ints ints = do
+    let med_idx = div (length (ints)) 2
+    if mod (length (ints)) 2 == 0
+      then (fromIntegral (ints!!med_idx - 1) + fromIntegral(ints!!(med_idx))) / 2
+      else fromIntegral (ints!!med_idx)
+
+get_median :: [String] -> Float
+get_median strings = get_median_ints (sort (convert_strings_to_lengths (strings) []))
+
+-- 11 elements list
+list_11 = ["icky", "panoramic", "new", "actually", "spotless", "town", "ready", "wrathful", "legs", "temporary", "brave"]
+
+-- 12 elements list
+list_12 = ["icky", "panoramic", "new", "actually", "spotless", "town", "ready", "wrathful", "legs", "temporary", "brave", "closed"]
+
+
 
 main :: IO ()
+-- Fibonacci testing
 --main = print(fib 20)
 --main = print(fib_tail 10 0 1)
-main = print(reverse_list [1..20] [])
+
+-- Reverse testing
+--main = print(reverse_list [1..20] [])
+
+-- List median testing
+--main = print(sort (convert_strings_to_lengths list_11 []))
+--main = print(sort (convert_strings_to_lengths list_12 []))
+
+main = do
+    print(sort (convert_strings_to_lengths list_11 []))
+    print(get_median list_11)
+
+    print(sort (convert_strings_to_lengths list_12 []))
+    print(get_median list_12)
