05dee9be1e6e8d5791428bc612d1850c13760726
Dmitry Chirin <chirin.dima@gmail.com>
2023-02-23 23:05:44 +0100

    fibonacci tail recursion semi working?

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,20 @@
 module Main (main) where
 
-import Lib
+import Lib ()
 
+-- Declaring functions here because lib's not working
 fib :: Int -> Int
 fib 0 = 0
 fib 1 = 1
 fib 2 = 1
-fib x = fib (x-1) + fib (x-2)
+fib x = fib (x - 1) + fib (x - 2)
+
+fib_tail :: Int -> Int -> Int -> Int
+fib_tail 0 a _ = a
+fib_tail 1 _ b = b
+--fib_tail n 1 1 = fib_tail (n - 1) 1 2
+fib_tail n a b = fib_tail (n - 1) b (a + b)
 
 main :: IO ()
-main = print(fib 20)
+--main = print(fib 20)
+main = print(fib_tail 10 0 1)
